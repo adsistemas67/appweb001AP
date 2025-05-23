c492c4eb9740336b6992b15b2f93fa4ab705b5d3
pine903618 <pine903618@S-PINE903618>
2025-05-23 09:16:55 -0500

    Agregar archivos de proyecto.

---
+++ appweb001/Models/ContextodeDatos.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace appweb001.Models
+{
+    public class ContextodeDatos: DbContext
+    {
+
+        public DbSet<Contactos> contactos { get; set; }
+
+        public ContextodeDatos(DbContextOptions<ContextodeDatos> options)
+            : base(options)
+        {
+        }
+
+
+
+    }
+}
